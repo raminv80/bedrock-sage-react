0710f233b4f1e3509a5f2092dd0164964be9f657
Ramin Vakilian <ramin@hybridmarketing.com.au>
2018-07-09 16:47:43 +0930

    remove comments from view

--- web/app/themes/bemore/resources/views/partials/content-single.blade.php
+++ web/app/themes/bemore/resources/views/partials/content-single.blade.php
@@ -9,5 +9,4 @@
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
 </article>
