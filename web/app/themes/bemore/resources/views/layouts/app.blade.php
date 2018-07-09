bf9d2fea510ac5c46618f1a250100e94ef5cd9a9
Ramin Vakilian <ramin@hybridmarketing.com.au>
2018-07-09 11:57:55 +0930

    Installs sage theme

---
+++ web/app/themes/bemore/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html @php language_attributes() @endphp>
+  @include('partials.head')
+  <body @php body_class() @endphp>
+    @php do_action('get_header') @endphp
+    @include('partials.header')
+    <div class="wrap container" role="document">
+      <div class="content">
+        <main class="main">
+          @yield('content')
+        </main>
+        @if (App\display_sidebar())
+          <aside class="sidebar">
+            @include('partials.sidebar')
+          </aside>
+        @endif
+      </div>
+    </div>
+    @php do_action('get_footer') @endphp
+    @include('partials.footer')
+    @php wp_footer() @endphp
+  </body>
+</html>
