6949bd5058cacfd0d6b0c8b7dd6144a827a7f99f
Ramin Vakilian <ramin@hybridmarketing.com.au>
2018-07-13 17:21:44 +0930

    Adds redux persist

--- web/app/themes/bemore/resources/assets/scripts/store/rootStore.js
+++ web/app/themes/bemore/resources/assets/scripts/store/rootStore.js
@@ -1,20 +1,31 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import createSagaMiddleware from "redux-saga";
 import logger from 'redux-logger'
-//import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootSaga from '../sagas/index.jsx';
 import * as reducers from "../reducers";
 
 const reducer = combineReducers(reducers);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
 
 const reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer,
+const store = createStore(persistedReducer,
   compose(applyMiddleware(sagaMiddleware, logger), reduxDevTools));
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+let persistor = persistStore(store)
+
+export { store, persistor };
--- web/app/themes/bemore/resources/assets/scripts/views/containers/App.jsx
+++ web/app/themes/bemore/resources/assets/scripts/views/containers/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from "react-redux";
 import Layout from './Layout.jsx';
-import store from '../../store/rootStore';
+import { store } from '../../store/rootStore';
 
 
 const App = ()=>(
